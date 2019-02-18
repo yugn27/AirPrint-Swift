ea40021ea7d201eac483166bd2b5ef3ae9b53142
Yash Nayak <yugn27@gmail.com>
2019-02-18 17:27:50 +0530

    Add files via upload

---
+++ AirPrint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AirPrint
+//
+//  Created by yashn on 29/09/18.
+//  Copyright © 2018 yashn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AirPrint/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AirPrint
+//
+//  Created by yashn on 29/09/18.
+//  Copyright © 2018 yashn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
